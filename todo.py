cecd79f34a8eaf36394644f2b8817e92c515d369
dheeraj009joshi <73873399+dheeraj009joshi@users.noreply.github.com>
2025-01-28 09:52:00 +0530

    Updated To-Do List on 2025-01-28

--- todo.py
+++ todo.py
@@ -1,7 +1,7 @@
 # Simple To-Do List CLI
 import sys
 
-tasks = ['Complete GitHub project', 'Write Python script', 'Fix bug in code', 'Task 30-1: Write Python script', 'Task 30-2: Complete GitHub project', 'Task 30-3: Task 30-1: Write Python script']
+tasks = ['Complete GitHub project', 'Write Python script', 'Fix bug in code', 'Task 30-1: Write Python script', 'Task 30-2: Complete GitHub project', 'Task 30-3: Task 30-1: Write Python script', 'Task 30-4: Task 30-1: Write Python script']
 
 def show_tasks():
     print("\nYour To-Do List:")
