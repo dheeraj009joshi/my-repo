7bac56adbe7eb43f4aae11400a6657d8e910994b
dheeraj009joshi <73873399+dheeraj009joshi@users.noreply.github.com>
2025-01-30 11:45:00 +0530

    Updated To-Do List on 2025-01-30

--- todo.py
+++ todo.py
@@ -1,7 +1,7 @@
 # Simple To-Do List CLI
 import sys
 
-tasks = ['Complete GitHub project', 'Write Python script', 'Fix bug in code', 'Task 30-1: Write Python script', 'Task 30-2: Complete GitHub project', 'Task 30-3: Task 30-1: Write Python script', 'Task 30-4: Task 30-1: Write Python script', 'Task 29-1: Task 30-2: Complete GitHub project', 'Task 29-2: Task 30-2: Complete GitHub project', 'Task 29-3: Task 30-1: Write Python script', 'Task 29-4: Task 29-1: Task 30-2: Complete GitHub project', 'Task 29-5: Task 30-2: Complete GitHub project', 'Task 29-6: Write Python script', 'Task 28-1: Task 29-3: Task 30-1: Write Python script']
+tasks = ['Complete GitHub project', 'Write Python script', 'Fix bug in code', 'Task 30-1: Write Python script', 'Task 30-2: Complete GitHub project', 'Task 30-3: Task 30-1: Write Python script', 'Task 30-4: Task 30-1: Write Python script', 'Task 29-1: Task 30-2: Complete GitHub project', 'Task 29-2: Task 30-2: Complete GitHub project', 'Task 29-3: Task 30-1: Write Python script', 'Task 29-4: Task 29-1: Task 30-2: Complete GitHub project', 'Task 29-5: Task 30-2: Complete GitHub project', 'Task 29-6: Write Python script', 'Task 28-1: Task 29-3: Task 30-1: Write Python script', 'Task 28-2: Task 29-2: Task 30-2: Complete GitHub project']
 
 def show_tasks():
     print("\nYour To-Do List:")
